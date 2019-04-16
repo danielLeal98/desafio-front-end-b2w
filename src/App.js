92848bb3ff6b7ab11f6b0628b22318a5b008c6c8
Yuri Mello <contato@yurimello.com>
2019-04-16 00:37:24 -0300

    adicionando rotas

--- src/App.js
+++ src/App.js
@@ -1,54 +1,18 @@
 import React, { Component } from 'react';
 import './App.scss';
 import planets from './services/planets'
+import planet from './components/Planets/planet';
+import {Switch, Route} from 'react-router-dom'
+
 
 class App extends Component {
 
-  constructor(props){
-    super(props)
-    this.state = {
-      planet:{
-        name: " ",
-        rotation_period: '',
-        orbital_period: '',
-        diameter:'',
-        climate:'',
-        gravity:'',
-        terrain: '',
-        surface_water: '',
-        population: '',
-        created: '',
-        edited: '',
-      }
-    }
-  }
-
-  getPlanetData = () => {
-    let idPlanet = Math.floor(Math.random() * 61 + 1);
-    planets.getPlanet(idPlanet).then(planet => {
-      this.setState({planet: planet.data})
-    })
-  };
-
-  componentWillMount() {
-    let idPlanet = Math.floor(Math.random() * 61 + 1);
-    planets.getPlanet(idPlanet).then(planet => {
-      this.setState({planet: planet.data})
-    })
-  }
-
   render() {
     return (
-      <div className="App">
-        <p>{this.state.planet.name}</p>
-        <p>{this.state.planet.climate}</p>
-        <p>{this.state.planet.gravity}</p>
-        <p>{this.state.planet.population}</p>
-        <p>{this.state.planet.diameter}</p>
-        <p>{this.state.planet.terrain}</p>
-        <div>
-        <input type="button" value="ATUALIZAR" onClick={this.getPlanetData}/>
-        </div>
+      <div>
+        <switch>
+          <Route path="/planet" component={planet} />
+        </switch>
       </div>
     );
   }
---
+++ src/components/Planets/planet.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import planets from './../../services/planets'
+
+class planet extends Component {
+
+    constructor(props){
+      super(props)
+      this.state = {
+        planet:{
+          name: '',
+          rotation_period: '',
+          orbital_period: '',
+          diameter:'',
+          climate:'',
+          gravity:'',
+          terrain: '',
+          surface_water: '',
+          population: '',
+          created: '',
+          edited: '',
+        }
+      }
+    }
+  
+    getPlanetData = () => {
+      let idPlanet = Math.floor(Math.random() * 61 + 1);
+      planets.getPlanet(idPlanet).then(planet => {
+        this.setState({planet: planet.data})
+      })
+    };
+  
+    componentDidMount() {
+        this.getPlanetData()
+    }
+  
+    render() {
+      return (
+        <div className="App">
+          <p>{this.state.planet.name}</p>
+          <p>{this.state.planet.climate}</p>
+          <p>{this.state.planet.gravity}</p>
+          <p>{this.state.planet.population}</p>
+          <p>{this.state.planet.diameter}</p>
+          <p>{this.state.planet.terrain}</p>
+          <div>
+          <input type="button" value="ATUALIZAR" onClick={this.getPlanetData} />
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  export default planet;
+  
