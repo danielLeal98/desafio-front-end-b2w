68773f67bdddba7feee7736629fd04fc38c5f1d3
Yuri Mello <contato@yurimello.com>
2019-04-19 17:02:51 -0300

    false no ignore case

---
+++ src/components/Start/Start.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import './start.scss';
+
+let startBanner = require('./../../assets/images/start-banner.png');
+
+class Start extends Component {
+
+    render() {
+        return (
+          <div className="startpage">
+            <img src={startBanner} alt="start banner" />
+            <div className="blink-start">Press Start</div>
+            <Link className="start-btn" to="/planet">START</Link>
+          </div>  
+        );
+    }
+}
+
+export default Start;
